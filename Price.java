02058ccbe6aa8f7e3886868ea78c6d127299fbfc
Marina <maarina.faria@gmail.com>
2021-03-13 01:34:14 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -1,14 +1,10 @@
 public abstract class Price {
-    private Rental _rental;
 
     public abstract int getPriceCode();
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((_rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-        && _rental.getDaysRented() > 1) return 2;
         return 1;
-
-     }
+    }
 }
 
