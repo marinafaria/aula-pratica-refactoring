d78726670792068eefb9cf1593368d3e1838b523
Marina <maarina.faria@gmail.com>
2021-03-13 02:32:59 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,5 +1,3 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
 
     public String getTitle(Customer aCustomer){
@@ -18,20 +16,5 @@ public class HtmlStatement extends Statement {
             + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) 
             + "</EM> frequent renter points<P>"
         );
-    }
-
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += each.getMovie().getTitle()+ ": " +
-                    String.valueOf(each.getCharge()) + "<BR>\n";
-        }
-        result +=  "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" +
-                String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-                "</EM> frequent renter points<P>";
-        return result;
     }
 }
--- Statement.java
+++ Statement.java
@@ -1,10 +1,20 @@
-public class Statement {
+import java.util.Enumeration;
 
-    // public abstract String getTitle();
-    
-    // while (rentals.hasMoreElements()) {
-    //     Rental each = (Rental) rentals.nextElement();
-    //     result += each.getMovie().getTitle()+ ": " +
-    //     String.valueOf(each.getCharge()) + "<BR>\n";
-    // }
+public abstract class Statement {
+
+    public abstract String getTitle(Customer aCustomer);
+    public abstract String getRentedMovies(Rental aRental);
+    public abstract String getCustomerInfo(Customer aCustomer);
+
+    public String value(Customer aCustomer){
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getTitle(aCustomer);
+        while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           result +=  getRentedMovies(each);
+        }
+        //add footer lines
+        result += getCustomerInfo(aCustomer);
+        return result;
+    }
 }
