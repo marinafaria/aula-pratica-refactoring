c5dfc5166be95fc0f56f7b6e6cdc943ab414db6f
Marina <maarina.faria@gmail.com>
2021-03-13 01:21:04 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -38,23 +38,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented){
-        double thisAmount = 0;
-        switch (_rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (_rental.getDaysRented() > 2)
-                    thisAmount += (_rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += _rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (_rental.getDaysRented() > 3)
-                    thisAmount += (_rental.getDaysRented() - 3) * 1.5;
-                    break;
-                    }
-            return thisAmount;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented){
--- Price.java
+++ Price.java
@@ -1,3 +1,25 @@
 public abstract class Price {
     public abstract int getPriceCode();
+    private Rental _rental;
+
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+        switch (_rental.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (_rental.getDaysRented() > 2)
+                    thisAmount += (_rental.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += _rental.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (_rental.getDaysRented() > 3)
+                    thisAmount += (_rental.getDaysRented() - 3) * 1.5;
+                    break;
+                    }
+            return thisAmount;
+    }
 }
+
