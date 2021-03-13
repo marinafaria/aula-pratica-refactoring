2167e2b50983155be66bd22ff832c7be065a4b98
Marina <maarina.faria@gmail.com>
2021-03-13 00:42:06 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -36,4 +36,11 @@ public class Rental {
       }
       return thisAmount;
    }
+
+   // add frequent renter points + add bonus for a two day new release rental
+   public int getFrequentRenterPoints() { 
+      if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+      && getDaysRented() > 1) return 2;
+      return 1;
+   }
 }
